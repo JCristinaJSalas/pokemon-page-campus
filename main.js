c3b237d609f630ffd8cfd28087c162f7d9bcaf1a
JCristinaJSalas <89360648+JCristinaJSalas@users.noreply.github.com>
2023-09-24 17:36:11 -0500

    feat: :sparkles: Add model to filter and show content

--- main.js
+++ main.js
@@ -1,8 +1,31 @@
 import { heroSection } from "./modulos/sweetAlert.js";
-import { slideSection } from "./modulos/slide.js";
+import { pokemonSection } from "./modulos/slide.js";
 
-const urlApi = "api-pokemon.json";
-addEventListener("DOMContentLoaded", async() => {
-  await heroSection(urlApi);
-  await slideSection(urlApi);
+const num = document.querySelector("#numPokemon");
+const botonBusqueda = document.querySelector("#mostrarPokemons");
+const contenedorPokemon = document.querySelector(".contenedor-pokemons");
+
+const urlApi = "https://pokeapi.co/api/v2/pokemon?limit=";
+addEventListener("DOMContentLoaded", async () => {
+
+  const urlApiLimit = urlApi + num.value;
+        await heroSection(urlApiLimit);
+        await pokemonSection(urlApiLimit);
+
+  num.addEventListener("keyup", async (e) => {
+    contenedorPokemon.innerHTML = "";
+
+    if (e.key === "Enter") {
+      const urlApiLimit = urlApi + num.value;
+      await heroSection(urlApiLimit);
+      await pokemonSection(urlApiLimit);
+    }
+  });
+
+  botonBusqueda.addEventListener("click", async (e) => {
+    contenedorPokemon.innerHTML = "";
+    const urlApiLimit = urlApi + num.value;
+    await heroSection(urlApiLimit);
+    await pokemonSection(urlApiLimit);
+  });
 });
