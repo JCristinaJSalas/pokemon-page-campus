222a066553e5dfe5f5ba1fd9983cf2b1810a89b7
JCristinaJSalas <89360648+JCristinaJSalas@users.noreply.github.com>
2023-09-25 00:30:58 -0500

    chore: :fire: organize the code

--- main.js
+++ main.js
@@ -7,21 +7,17 @@ const contenedorPokemon = document.querySelector(".contenedor-pokemons");
 
 const urlApi = "https://pokeapi.co/api/v2/pokemon?limit=";
 addEventListener("DOMContentLoaded", async () => {
-
   const urlApiLimit = urlApi + num.value;
         await heroSection(urlApiLimit);
         await pokemonSection(urlApiLimit);
-
   num.addEventListener("keyup", async (e) => {
     contenedorPokemon.innerHTML = "";
-
     if (e.key === "Enter") {
       const urlApiLimit = urlApi + num.value;
       await heroSection(urlApiLimit);
       await pokemonSection(urlApiLimit);
     }
   });
-
   botonBusqueda.addEventListener("click", async (e) => {
     contenedorPokemon.innerHTML = "";
     const urlApiLimit = urlApi + num.value;
