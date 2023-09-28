ce0e7df4dafae480d79196e00152853c3898d432
JCristinaJSalas <89360648+JCristinaJSalas@users.noreply.github.com>
2023-09-27 20:44:34 -0500

    refactor: :recycle: Improve the code and put a better modules

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
-import { heroSection } from "./modulos/sweetAlert.js";
-import { pokemonSection } from "./modulos/slide.js";
-import { busqueda } from "./modulos/busqueda.js";
+//import { busqueda } from "./modulos/busqueda.js";
+import { mostrarTarjeta } from "./modulos/mostrarTarjeta.js";
 
 const num = document.querySelector("#numPokemon");
 const botonBusqueda = document.querySelector("#mostrarPokemons");
@@ -12,25 +11,23 @@ const urlApi = "https://pokeapi.co/api/v2/pokemon?limit=";
 
 
 addEventListener("DOMContentLoaded", () => {
-  const urlApiLimit = urlApi + num.value;
-        heroSection(urlApiLimit);
-        pokemonSection(urlApiLimit);
-        busqueda(url)
 
-  num.addEventListener("keyup", async (e) => {
+  const urlApiLimit = urlApi + num.value;
+  mostrarTarjeta(urlApiLimit)
+ // busqueda(url)
+
+ /*  num.addEventListener("keydown", async (e) => {
     contenedorPokemon.innerHTML = "";
     if (e.key === "Enter") {
       const urlApiLimit = urlApi + num.value;
-      await heroSection(urlApiLimit);
-      await pokemonSection(urlApiLimit);
-      await busqueda(url)
+      await mostrarTarjeta(urlApiLimit);
+    //  await busqueda(url)
     }
   });
   botonBusqueda.addEventListener("click", async (e) => {
     contenedorPokemon.innerHTML = "";
     const urlApiLimit = urlApi + num.value;
-    await heroSection(urlApiLimit);
-    await pokemonSection(urlApiLimit);
-    await busqueda(url)
-  });
+    await mostrarTarjeta(urlApiLimit)
+   // await busqueda(url)
+  }); */
 });
--- modulos/mostrarTarjeta.js
+++ modulos/mostrarTarjeta.js
@@ -1,19 +1,24 @@
-import { alertPokemon } from "./alert-boton.js";
+import { mostrarApi, mostrarApiPokemon } from "./apiFunctions.js";
+import { alertPokemon } from "./alert-pokemon.js";
+
 const contenedorPokemon = document.querySelector(".contenedor-pokemons");
 
-export const mostrarTarjeta = async (individualData) => {
-  let imagenPokemon = individualData.sprites.front_default;
+export const mostrarTarjeta = async (urlApiLimit) => {
+  const data = await mostrarApi(urlApiLimit);
+  data.forEach(async (element) => {
+    const individualData = await mostrarApiPokemon(element);
 
-  const urlColor = await (await fetch(individualData.species.url)).json();
-  const color = urlColor.color.name;
-  const estiloCaja = `color: ${color};border: 1.5px solid ${color};`;
+    let imagenPokemon = individualData.sprites.front_default;
+    const urlColor = await (await fetch(individualData.species.url)).json();
+    const color = urlColor.color.name;
+    const estiloCaja = `color: ${color};border: 1.5px solid ${color};`;
 
-  const contenedor = document.createElement("div");
-  contenedor.className = "contenedor-pokemon";
-  contenedor.id = individualData.id;
-  contenedor.style.cssText = estiloCaja;
+    const contenedor = document.createElement("div");
+    contenedor.className = "contenedor-pokemon";
+    contenedor.id = individualData.id;
+    contenedor.style.cssText = estiloCaja;
 
-  contenedor.innerHTML = `
+    contenedor.innerHTML = `
     <div class="contenedor-img">
       <img src="${imagenPokemon}" alt="Pokemon ${individualData.name}" />
     </div>
@@ -21,8 +26,9 @@ export const mostrarTarjeta = async (individualData) => {
       <h3>${individualData.name.toUpperCase()}</h3>
     </div>
   `;
-  contenedorPokemon.appendChild(contenedor);
-  contenedor.addEventListener("click", async () => {
-    alertPokemon(individualData);
+    contenedorPokemon.appendChild(contenedor);
+    contenedor.addEventListener("click", async () => {
+      alertPokemon(individualData);
+    });
   });
 };
