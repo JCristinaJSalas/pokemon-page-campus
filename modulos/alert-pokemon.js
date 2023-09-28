cd615050b0132a6a27c84273c6da800530380187
JCristinaJSalas <89360648+JCristinaJSalas@users.noreply.github.com>
2023-09-27 23:37:19 -0500

    style: :lipstick: Add more styles to the Tarjeta

--- modulos/alert-pokemon.js
+++ modulos/alert-pokemon.js
@@ -1,27 +1,31 @@
 import { simbolo } from "./tipoPokemonImg.js";
+
 export const alertPokemon = async (info) => {
   try {
     let colorUrl = info.species.url;
     const colorData = await (await fetch(colorUrl)).json();
     const color = colorData.color.name;
-    console.log("info", color);
     // objetos de estilos
     const colorEstilo = `color: ${color}; border-bottom: 2px solid ${color}`;
     const colorEstiloTipo = `color: ${color}; border: 2px solid ${color}; padding: 8px; margin: 8px`;
 
     let imagenPokemon = info.sprites.front_default;
-    let peso = info.weight;
+    let estatus = info.stats;
     let experiencia = info.base_experience;
-    let altura = info.height;
     let tipoPoke = info.types.map((i) => i.type).map((tipo) => tipo.name);
     let tipoTexto = tipoPoke.map(
       (nombre) =>
         `<h4 class="tipo" style="${colorEstiloTipo}">${nombre.toUpperCase()}</h4>`
     );
-    const fotoTipo = await simbolo(tipoPoke);
-    console.log(fotoTipo);
+    const fotoTipo = simbolo(tipoPoke);
+
     Swal.fire({
       html: `
+      <style>
+        input[type='range']::-webkit-slider-thumb {
+          background-color: ${color}; opacity : 0.75;
+        
+      </style>
       <div class="contenedor-pokemon-alerta">
         <div class="contenedor-logo">
           <h4 class="circulo-id" style="${colorEstiloTipo}">${info.id}</h4>
@@ -31,15 +35,24 @@ export const alertPokemon = async (info) => {
         <div class="contenedor-info-alerta">
           <div class="contenedor-titulo-alerta">
             <h3 class="titulo-alerta" style="${colorEstilo}">${info.name.toUpperCase()}</h3>
-            <h5><i class='bx bx-bar-chart icono'></i> ${experiencia} HP</h5>
+            <h5><i class='bx bx-bar-chart icono'></i> ${experiencia} EXP</h5>
           </div>
 
           <div class="contenedor-texto-alerta">
-            <h5><i class='bx bx-bug icono'></i> ${peso}</h5>
-            <h5><i class='bx bx-vertical-top icono'></i> ${altura}</h5>
+            ${estatus
+              .map(
+                (e) => /*html*/ `
+              <div class="container">
+                <h4>${e.stat.name}</h4>
+                <input class="rango" type="range" value="${e.base_stat}" data-stat="${e.stat.name}" max="150">
+                <label data-stat="${e.stat.name}" id="${e.stat.name}">
+                    ${e.base_stat}/150 
+                </label>
+              </div>`
+              )
+              .join("")}
           </div>
-          <hr>
-          <div class="contenedor-tipo-alerta">${tipoTexto.join('')}</div>
+          <div class="contenedor-tipo-alerta">${tipoTexto.join("")}</div>
         </div>
         <div class="contenedor-img-alerta">
           <img src="${imagenPokemon}" alt="Pokemon ${info.name}" />
@@ -49,7 +62,20 @@ export const alertPokemon = async (info) => {
       color: "#000",
       width: "390px",
     });
+
+    const inputRanges = document.querySelectorAll(".rango");
+    inputRanges.forEach((inputRange) => {
+      inputRange.style.background = color; 
+    });
   } catch (error) {
     console.log("Error en la alerta", error);
   }
+
+  document.addEventListener("input", (e) => {
+    if (e.target.matches(".rango")) {
+      const statName = e.target.dataset.stat;
+      const label = document.querySelector(`#${statName}`);
+      label.innerText = `${e.target.value}/150`;
+    }
+  });
 };
