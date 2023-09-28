0218e58e0d94f1231e8751865cca43e0ab7ab532
JCristinaJSalas <89360648+JCristinaJSalas@users.noreply.github.com>
2023-09-28 01:10:08 -0500

    feat: :sparkles: Add Read,Write, Evalute functions for Mockapi
    Se crea las funciones leerMockapi,escribirMockapi y evaluaMockapi

--- modulos/apiFunctions.js
+++ modulos/apiFunctions.js
@@ -1,20 +1,54 @@
 // leera el .json
 export const mostrarApi = async (urlApiLimit) => {
-    try {
-      const data = await (await fetch(urlApiLimit)).json();
-      return data.results;
-    } catch (error) {
-      console.log("No se cargo la API", error);
-    }
+  try {
+    const data = await (await fetch(urlApiLimit)).json();
+    return data.results;
+  } catch (error) {
+    console.log("No se cargo la API", error);
+  }
+};
+
+export const mostrarApiPokemon = async (element) => {
+  try {
+    const pokemon = await (await fetch(element.url)).json();
+    return pokemon;
+  } catch (error) {
+    console.log("Error al leer la url del pokemon", error);
+  }
+};
+
+export const leerMockapi = async (urlMockapi) => {
+  const dataMockapi = await (await fetch(urlMockapi)).json();
+  return dataMockapi;
+};
+
+
+const escribirMockapi = async (urlMockApi,name,especificaciones) => {
+  const newData = especificaciones.map((info) => {
+    const base = info.base_stat;
+    const name = info.stat.name;
+    const objecto = { name, base };
+    return objecto;
+  });
+  const newDataPoke = {name, especificaciones:newData}
+  const config = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(newDataPoke),
   };
-  
-  export const mostrarApiPokemon = async (element) => {
-    try {
-      const pokemon = await (await fetch(element.url)).json();
-      return pokemon;
-    } catch (error) {
-      console.log("Error al leer la url del pokemon", error);
-    }
-  };
-  
-  
+  await (await fetch(urlMockApi, config)).json();
+};
+
+
+
+export const evaluaMockapi = async (urlMockApi, name, especificaciones) => {
+  const nameMockapi = (await leerMockapi(urlMockApi)).map((e) => e.name);
+  nameMockapi.includes(name)
+    ? console.log("Ya esta, No se agrega")
+    : escribirMockapi(urlMockApi, name, especificaciones);
+};
+
+
+
